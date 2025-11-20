245a78e259268f5812d5d5c7b7dc05a120d8167e
michfish966 <michfish966@ed.amdsb.ca>
2025-11-20 10:32:26 -0500

    Mike

--- Quiz01-IO&Variables/Form1.cs
+++ Quiz01-IO&Variables/Form1.cs
@@ -2,6 +2,7 @@ namespace Quiz01_IO_Variables
 {
     public partial class Form1 : Form
     {
+        string userName;
         public Form1()
         {
             InitializeComponent();
@@ -20,12 +21,28 @@ namespace Quiz01_IO_Variables
         private void button1_Click_1(object sender, EventArgs e)
         {
             MessageBox.Show("To output two decimal places use F2");
-           
+
         }
 
         private void button2_Click(object sender, EventArgs e)
         {
             MessageBox.Show("4 items at 2.25 each with tax will be $10.17");
+        }
+
+        private void q01Button_Click(object sender, EventArgs e)
+        {
+            q1OutputLabel.Text = "Go Bears!";
+        }
+
+        private void q02Button_Click(object sender, EventArgs e)
+        {
+            userName = q2TextBox.Text;
+            q2OutputLabel.Text = "Go, " + userName + "!";
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
